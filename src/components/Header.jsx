f3a4b6bed27f9b3743f1ba7b1749ce5992200a8f
josuem317 <jowebdesigc@gmail.com>
2024-12-11 12:39:09 -0400

    modifi rules eslint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,9 +17,11 @@ export default function Header() {
             <header className="bg-black">
                 <nav className="border-gray-200 bg-black dark:border-gray-700">
                     <div className="max-w-screen-lg flex flex-wrap items-center justify-around mx-auto p-4">
-                        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                        <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                            <a href="/">
                             <img src="/logo.png" className="h-36" alt="Flowbite Logo" />
-                        </a>
+                            </a>
+                        </Link>
                         <button data-collapse-toggle="navbar-multi-level" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-multi-level" aria-expanded="false" onClick={navMobileControl}>
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -29,7 +31,9 @@ export default function Header() {
                         <div className={`${activeStyles} w-full md:block md:w-auto`} id="navbar-multi-level">
                             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-black md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-black dark:border-gray-700">
                                 <li>
-                                    <a href="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:p-0 hover:text-blue-600 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Inicio</a>
+                                    <Link href="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:p-0 hover:text-blue-600 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">
+                                    <a href="/">Inicio</a>
+                                    </Link>
                                 </li>
                                 <li>
                                     <Link href="/Productos" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Productos</Link>

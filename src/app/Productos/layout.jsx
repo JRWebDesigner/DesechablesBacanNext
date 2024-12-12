679a3c68c23a52db2c41b7cb193c0167aadf7015
josuem317 <jowebdesigc@gmail.com>
2024-12-12 11:34:09 -0400

    bugs 3

--- src/app/Productos/layout.jsx
+++ src/app/Productos/layout.jsx
@@ -11,17 +11,17 @@ export default function Layout({ children }) {
 					<AccordionItem key="1" aria-label="Accordion 1" title="Buscar por Marcas">
 						<nav>
 							<ul className="gap-4 overflow-y-scroll h-[170px] p-3">
-								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/productos/">Todos</Link></li>
-								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"  ><Link className="block" href="/productos/advan">Advan</Link></li>
-								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/productos/belen">Belen</Link></li>
-								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/productos/darnel">Darnel</Link></li>
-								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/productos/delSur">Del Sur</Link></li>
-								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/productos/docplast">Docplast</Link></li>
-								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/productos/fiveStick">Five Stick</Link></li>
-								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/productos/feiteng">Feiteng</Link></li>
-								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/productos/isoplast">Isoplast</Link></li>
-								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/productos/pastenPlast">Pasten Plast</Link></li>
-								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/productos/sanJorge">San Jorge</Link></li>
+								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/Productos/">Todos</Link></li>
+								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"  ><Link className="block" href="/Productos/advan">Advan</Link></li>
+								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/Productos/belen">Belen</Link></li>
+								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/Productos/darnel">Darnel</Link></li>
+								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/Productos/delSur">Del Sur</Link></li>
+								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/Productos/docplast">Docplast</Link></li>
+								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/Productos/fiveStick">Five Stick</Link></li>
+								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/Productos/feiteng">Feiteng</Link></li>
+								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/Productos/isoplast">Isoplast</Link></li>
+								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/Productos/pastenPlast">Pasten Plast</Link></li>
+								<li className="border-2 border-b-gray-200 hover:bg-blue-600 hover:text-white focus:bg-blue-600 before:text-white"><Link className="block" href="/Productos/sanJorge">San Jorge</Link></li>
 								
 							</ul>
 						</nav>

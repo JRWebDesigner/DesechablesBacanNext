a55b73e3980fab657336b03248b23d3d7a3317bf
josuem317 <jowebdesigc@gmail.com>
2024-12-12 11:30:11 -0400

    bugs 2

--- src/app/Productos/page.jsx
+++ src/app/Productos/page.jsx
@@ -57,7 +57,7 @@ export default function Productos() {
   return (
         <>
 			<h1 className="text-center uppercase font-bold text-3xl mt-10">Todos nuestros productos</h1>
-            <div className="flex flex-wrap justify-center items-center gap-7 m-10">
+            <div className="flex flex-row-reverse flex-wrap justify-center items-center gap-7 m-10">
             {
                 products.map((prod)=>(
                     <div 

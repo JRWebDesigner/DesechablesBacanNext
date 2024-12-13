a1244876743ba1bcd181a693389cc05a8ea1e27e
josuem317 <jowebdesigc@gmail.com>
2024-12-13 10:35:24 -0400

    add modal produvt

--- src/app/Productos/page.jsx
+++ src/app/Productos/page.jsx
@@ -1,7 +1,19 @@
 "use client"
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import products from './products.json'
 export default function Productos() {
+  const [modalOpen, setModalOpen] = useState(false); 
+  const [selectedProduct, setSelectedProduct] = useState(null); 
+
+  const openModal = (product) => {
+    setSelectedProduct(product);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setSelectedProduct(null);
+  };
   useEffect(() => {
     const translates = document.querySelectorAll('.translate-y-14');
     const scales = document.querySelectorAll('.scale-50');
@@ -56,33 +68,59 @@ export default function Productos() {
   }, []);
   return (
         <>
-			<h1 className="text-center uppercase font-bold text-3xl mt-10">Todos nuestros productos</h1>
-            <div className="flex flex-row-reverse flex-wrap justify-center items-center gap-16 m-10">
-            {
-                products.map((prod)=>(
-                    <div 
-      key={prod.id} 
-      className="flex flex-col justify-center items-center shadow-xl w-[240px] p-4 text-xl font-bold gap-3 rounded-3xl hover:scale-[1.05] hover:bg-gray-100 duration-500 translate-y-14 scale-50"
-    >
-      <img 
-        src={prod.img} 
-        alt={prod.name} 
-      />
-      <span>
-        {prod.name}
-      </span>
-      <small 
-        className="text-black uppercase"
-      >
-        {prod.mark}
-      </small>
-      <div className="w-full flex justify-around items-center text-sm gap-3 font-semibold">
-        <button className="bg-green-400 p-2 rounded-xl hover:scale-110">AÑADIR</button>
+			<h1 className="text-center uppercase font-bold text-3xl mt-10">
+        Todos nuestros productos
+      </h1>
+      <div className="flex flex-row-reverse flex-wrap justify-center items-center gap-16 m-10">
+        {products.map((prod) => (
+          <div
+            key={prod.id}
+            onClick={() => openModal(prod)}
+            className="flex flex-col justify-center items-center shadow-xl w-[240px] p-4 text-xl font-bold gap-3 rounded-3xl hover:scale-[1.05] hover:bg-gray-100 duration-500 translate-y-14 scale-50 cursor-pointer"
+          >
+            <img src={prod.img} alt={prod.name} />
+            <span>{prod.name}</span>
+            <small className="text-black uppercase">{prod.mark}</small>
+          </div>
+        ))}
       </div>
-    </div>
-                ))
-            }
+      {modalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 overflow-y-auto">
+          <div className="bg-white p-6 rounded-lg shadow-lg flex flex-wrap justify-center items-center gap-7">
+            <img
+              src={selectedProduct.img}
+              alt={selectedProduct.name}
+              className="w-[300px] md:w-[400px] h-auto rounded"
+            />
+            <div className='w-[90%] max-w-[500px] min-w-[300px] flex justify-center items-center flex-col'>
+            <h2 className="text-2xl font-bold mt-4">{selectedProduct.name}</h2>
+            <small className="text-gray-700 mt-2 uppercase">{selectedProduct.mark}</small>
+            <div>
+              <h3>Descripción</h3>
+              <p>
+              {selectedProduct.descrip}
+              </p>
             </div>
+            <div className="mt-4 flex justify-end">
+              <button
+                onClick={closeModal}
+                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+              >
+                Cerrar
+              </button>
+              <a
+              href={`https://wa.me/+59168071168?text=Hola!%20Estoy%20interesado%20en%20${selectedProduct.name}`}
+              target='_blank'
+                onClick={closeModal}
+                className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+              >
+                Comprar
+              </a>
+            </div>
+            </div>
+          </div>
+        </div>
+      )}
         </>
     )
 }

cca89d0ebbc5031ca4678b00cc467d62fafed029
josuem317 <jowebdesigc@gmail.com>
2024-12-16 09:26:49 -0400

    remove hover nav mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,13 +34,13 @@ export default function Header() {
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link href="/Productos" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Productos</Link>
+                                    <Link href="/Productos" className="block py-2 px-3 text-white bg-transparent rounded md:bg-transparent md:p-0 md:dark:bg-transparent dark:hover:bg-gray-700">Productos</Link>
                                 </li>
-                                <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                                <li className="block py-2 px-3 text-white bg-transparent rounded md:bg-transparent md:p-0 md:dark:bg-transparent dark:hover:bg-gray-700">
                                     <Link href="/Carrito">Mi Carrito</Link>
                                 </li>
                                 <li>
-                                    <Link href="/Contactos" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contactos</Link>
+                                    <Link href="/Contactos" className="block py-2 px-3 text-white bg-transparent rounded md:bg-transparent md:p-0 md:dark:bg-transparent dark:hover:bg-gray-700">Contactos</Link>
                                 </li>
                                 
                             </ul>

02c109c468717368ac3bcf72a86844a42be3a11a
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-02-21 14:26:20 -0400

    Update carrito number WhatsApp

--- src/app/Carrito/page.jsx
+++ src/app/Carrito/page.jsx
@@ -108,7 +108,7 @@ export default function Carrito() {
         <h2 className="text-center text-lg md:text-xl font-semibold">
         Total: {total} Bs.
       </h2>
-        <a href={`https://wa.me/+59163524798?text=${message}`}
+        <a href={`https://wa.me/+59173534369?text=${message}`}
               target='_blank' className="bg-green-500 p-2 text-white rounded-full hover:bg-green-600 flex justify-center items-center text-sm md:text-lg">
           COMPRAR AHORA <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-camper"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 18a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M15 18a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M5 18h-1a1 1 0 0 1 -1 -1v-11a2 2 0 0 1 2 -2h12a4 4 0 0 1 4 4h-18" /><path d="M9 18h6" /><path d="M19 18h1a1 1 0 0 0 1 -1v-4l-3 -5" /><path d="M21 13h-7" /><path d="M14 8v10" /></svg>
         </a>

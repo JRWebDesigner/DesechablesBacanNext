ebcc59d0171ba8865ee7add6e486e657727d9ee9
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-04-27 07:38:33 -0400

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -45,7 +45,7 @@ export default function Modal({ product, closeModal }) {
 
         {/* Contenedor de información con scroll si es necesario */}
         <div className='w-full max-w-[500px] min-w-[300px] flex justify-center items-center flex-col overflow-y-auto'>
-          <h2 className="text-2xl font-bold mt-4 text-center">{product.name} ----- {product.price}Bs. / {product.xmayor}Bs. por mayor </h2>
+          <h2 className="text-2xl font-bold mt-4 text-center">{product.name} ----- {product.price}Bs.</h2>
           <small className="text-gray-700 mt-2 uppercase text-center">{product.mark} ----- {product.categoryName}</small>
           <div className="flex items-start flex-col w-full mt-4">
             <h3 className="font-bold">Descripción</h3>
@@ -61,7 +61,7 @@ export default function Modal({ product, closeModal }) {
               Añadir <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 17h-11v-14h-2" /><path d="M6 5l14 1l-1 7h-13" /></svg>
             </button>
             <a
-              href={`https://wa.me/+59173534369?text=Hola!%20Estoy%20interesado%20en%20${product.name}%20,marca%20${product.mark}`}
+              href={`https://wa.me/+59173534369?text=Hola!%20Estoy%20interesado%20en%20${product.name}%20,marca%20${product.mark},%20precio:${product.price}`}
               target='_blank'
               rel="noopener noreferrer"
               onClick={closeModal}
